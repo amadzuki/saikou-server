0ad08a2533ff482a437abb1c9196c7c1a1f61e5b
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-08-27 15:56:52 +0700

    Minor fix updateUserProfile

--- middlewares/users/controller.js
+++ middlewares/users/controller.js
@@ -51,7 +51,7 @@ module.exports = {
 
     const updatedUser = await User.findByIdAndUpdate(mongoId, body, {
       new: true,
-      select: '-hash -_id',
+      select: '-hash -_id -__v -updatedAt',
     })
 
     res.status(200).send({
